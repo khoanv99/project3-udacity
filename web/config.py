a58ae6be31f55ae24275ff70a03f3eafc4cd8d85
rickboi9z <khoanv1999@gmail.com>
2023-10-28 23:26:19 +0700

    add

---
+++ web/config.py
@@ -0,0 +1,24 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="duynd-dbserver.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="adminserver@duynd-dbserver" #TODO: Update value
+    POSTGRES_PW="Ahihicode@123"   #TODO: Update value
+    POSTGRES_DB="duynd-dbserver"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://duynd-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Pt/chF1B+B1jof7GMjSb8Y+ovw8/GSzT6+ASbAOaW3Y=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
