f3f3390f4fa10f3d99e5388d697cec5432cc528f
khoanv99 <81483006+khoanv99@users.noreply.github.com>
2023-10-29 08:04:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     DEBUG = True
     POSTGRES_URL="duynd-dbserver.postgres.database.azure.com"  #TODO: Update value
-    POSTGRES_USER="adminserver@duynd-dbserver" #TODO: Update value
+    POSTGRES_USER="adminserver" #TODO: Update value
     POSTGRES_PW="Ahihicode@123"   #TODO: Update value
-    POSTGRES_DB="duynd-dbserver"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
